2fcef6edd593a1e1a25b92a37f99a0489c4d096f
Min <als904204@naver.com>
2023-12-12 00:23:49 +0900

    feat:커스텀된 예외메세지, 테스트용 RestController

--- src/main/java/com/example/exceptionaop/controller/HelloController.java
+++ src/main/java/com/example/exceptionaop/controller/HelloController.java
@@ -1,5 +1,31 @@
 package com.example.exceptionaop.controller;
 
+import com.example.exceptionaop.global.exception.CustomException;
+import com.example.exceptionaop.global.exception.ExceptionCode.LogicExceptionCode;
+import com.example.exceptionaop.global.exception.ExceptionCode.ValidationExceptionCode;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/error")
 public class HelloController {
 
+    @GetMapping("/logic")
+    public String errorPage1() {
+        if (true) {
+            throw new CustomException(LogicExceptionCode.BAD_REQUEST);
+        }
+        return "error";
+    }
+
+    @GetMapping("/validate")
+    public String errorPage2() {
+        if (true) {
+            throw new CustomException(ValidationExceptionCode.INVALID_STATE);
+        }
+        return "error";
+    }
 }
+
